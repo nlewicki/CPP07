ad9127ef038677a9a4dbc2e55f3756a4199983f6
Nico <nlewicki@student.42heilbronn.de>
2025-03-17 11:08:01 +0100

    added iter template

---
+++ ex01/iter.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 11:03:41 by nlewicki          #+#    #+#             */
+/*   Updated: 2025/03/17 11:05:12 by nlewicki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <iostream>
+
+template <typename T>
+void	iter(T *array, size_t size, void (*f)(T const &))
+{
+	for (size_t i = 0; i < size; i++)
+		f(array[i]);
+};
+
+template <typename T>
+void	print(T const &i)
+{
+	std::cout << i<< std::endl;
+};
+
+#endif
