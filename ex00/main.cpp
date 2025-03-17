8ce52fbcb8bbf9ccc81f9e1eeccaa2aee0e27361
Nico <nlewicki@student.42heilbronn.de>
2025-03-17 11:03:13 +0100

    Added first templates, swap, min and max

---
+++ ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 10:54:28 by nlewicki          #+#    #+#             */
+/*   Updated: 2025/03/17 11:01:57 by nlewicki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+#include <iostream>
+
+int	main( void )
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
---
+++ ex00/whatever.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 10:54:44 by nlewicki          #+#    #+#             */
+/*   Updated: 2025/03/17 10:56:16 by nlewicki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T>
+void	swap(T &a, T &b)
+{
+	T	tmp = a;
+	a = b;
+	b = tmp;
+};
+
+template <typename T>
+T		&min(T &a, T &b)
+{
+	return (a < b ? a : b);
+};
+
+template <typename T>
+T		&max(T &a, T &b)
+{
+	return (a > b ? a : b);
+};
+
+#endif
